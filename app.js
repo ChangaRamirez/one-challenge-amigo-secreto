370e15f4b382f7b15d2198012569266e2695e905
Changa Ramirez <laloraco@hotmail.com>
2025-01-24 19:04:04 -0600

    Implementa funcion para agregar amigos

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
+//Inicializando elementos del DOM
+const inputAmigo = document.getElementById("amigo");
+
 //Inicializando variables
 const nombres = [];
+
+//Definiendo funciones
+function agregarAmigo(){
+  if (!inputAmigo.value) { //Aquí revisamos si el campo de input está vacío
+    alert("Por favor, inserte un nombre");
+    return; //De ser así, una alerta se muestra al usuario y terminamos la función
+  } else {
+    nombres.push(inputAmigo.value);
+    inputAmigo.value = "";
+    console.log(nombres); //Test de que los strings se almacenan correctamente en nombres
+  }
+}
